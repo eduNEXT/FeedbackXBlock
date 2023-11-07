d43a702b5df56b4469c87d7748cc3c556cf6108c
Cristhian Garcia <crisgarta8@gmail.com>
2023-11-07 15:45:21 -0500

    chore: add pylint disable

--- feedback/settings/test.py
+++ feedback/settings/test.py
@@ -5,7 +5,7 @@ https://docs.djangoproject.com/en/2.22/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.22/ref/settings/
 """
-from workbench.settings import *
+from workbench.settings import *  # pylint: disable=wildcard-import
 
 from django.conf.global_settings import LOGGING
 
